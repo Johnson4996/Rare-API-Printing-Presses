e5248ab2bdd6184cb37432ad31305d10b1a4d709
ardisrtilson <email@example.com>
2020-11-16 18:20:31 -0600

    User number now in local storage

--- rareapi/views/profiles.py
+++ rareapi/views/profiles.py
@@ -15,29 +15,9 @@ class Profile(ViewSet):
     def list(self, request):
             """Handle get requests for profiles """
 
-            profiles = RareUser.objects.all().order_by('user')  
-
+            profiles = RareUser.objects.all().order_by('user')
             serializer = ProfileSerializer(profiles, many=True, context={'request': request})
             return Response(serializer.data)
-
-    # def retrieve(self, request, pk=None):
-    #         """Handle GET requests for single profile
-    #         Returns:
-    #             Response -- JSON serialized profile instance
-    #         """
-    #         SingleProfile = RareUser.objects.get(pk=pk)
-            
-
-    #         try:
-    #             RareUser.objects.get(user=request.auth.user, pk=(SingleProfile.id)
-    #             SingleProfile.IsAdmin = "Admin"
-    #         except RareUser.DoesNotExist:
-    #             SingleProfile.IsAdmin = "Author"
-    #         try:
-    #             serializer = ProfileSerializer(SingleProfile, context={'request': request})
-    #             return Response(serializer.data)
-    #         except Exception as ex:
-    #             return HttpResponseServerError(ex)
 
 class ProfileUserSerializer(serializers.ModelSerializer):
 
@@ -52,5 +32,5 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = RareUser
         fields = ('id', 'bio', 'profile_image_url', 'created_on',
-                'active', 'user' )
+                'active', 'user')
         depth = 1

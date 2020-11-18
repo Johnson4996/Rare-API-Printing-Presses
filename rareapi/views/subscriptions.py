201c555b47577a3034cc2fbd69b0fde8c39c7796
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-11-18 11:40:03 -0600

    put action method in viewset

--- rareapi/views/subscriptions.py
+++ rareapi/views/subscriptions.py
@@ -32,6 +32,66 @@ class Subs(ViewSet):
             subscription, many=True, context={'request': request})
         return Response(serializer.data)
 
+    @action(methods=['get', 'post', 'delete'], detail=True)
+    def follow(self, request, pk=None):
+        """Managing followers subscribing to authors"""
+
+        # A user wants to subscribe to an author
+        if request.method == "POST":
+            subs = Subscriptions.object.get(pk=pk)
+
+        # Django uses the `Authorization` header to determine
+        # which user is making the request to subscribe
+
+            author = RareUser.objects.get(user=request.auth.user)
+
+            try:
+        #Determine if the user is already following
+                subscribe = Subscriptions.objects.get(
+                    follower_id=user_id) #will need help with this
+                return Response(
+                    {'message': 'You are already subscribed to this Author'},
+                    status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+            except Subscriptions.DoesNotExist:
+            # The user is not signed up
+                subscribe = Subscriptions()
+                subscribe.subs = subs
+                subscribe.author = author
+                subscribe.save()
+
+                return Response({}, status=status.HTTP_201_CREATED)
+
+        # User wants to Unsubscribe
+        elif request.method == "DELETE":
+            # handle the case if the client wants to 
+            # unfollow
+            try:
+                subs = Subscriptions.objects.get(pk=pk)
+            except Subscriptions.DoesNotExist:
+                return Response(
+                    {'message': 'You do not follow this author'},
+                    status=status.HTTP_400_BAD_REQUEST)
+
+        # Get the authenticated user
+            follower = RareUser.objects.get(user=request.auth.user)
+
+            try:
+                # try to unsubscribe
+                subs = Subscriptions.objects.get(
+                    follower_id=user_id) # will need help with this
+                subs.delete()
+                return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+            except Subscriptions.DoesNotExist:
+                return Response(
+                    {'message': 'Not currently subscribed to author'},
+                    status=status.HTTP_404_NOT_FOUND)
+
+    # If the client performs a request with a method that isnt
+    # POST or DELETE, tell the client the method is not supported
+        return Response({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
 class SubsUserSeralizer(serializers.ModelSerializer):
     """JSON serializer for subscriptions"""
 
@@ -55,63 +115,4 @@ class SubSerializer(serializers.HyperlinkedModelSerializer):
         fields =('id', 'created_on', 'ended_on', 'follower_id', 'author_id', 'author')
         depth = 1
 
-
-@action(methods=['get', 'post', 'delete'], detail=True)
-def follow(self, request, pk=None):
-    """Managing followers subscribing to authors"""
-
-    # A user wants to subscribe to an author
-    if request.method == "POST":
-        subs = Subscriptions.object.get(pk=pk)
-
-        # Django uses the `Authorization` header to determine
-        # which user is making the request to subscribe
-
-        author = RareUser.objects.get(user=request.auth.user)
-
-        try:
-            #Determine if the user is already following
-            subscribe = Subscriptions.objects.get(
-                follower_id=user_id) #will need help with this
-            return Response(
-                {'message': 'You are already subscribed to this Author'},
-                status=status.HTTP_422_UNPROCESSABLE_ENTITY
-            )
-        except Subscriptions.DoesNotExist:
-            # The user is not signed up
-            subscribe = Subscriptions()
-            subscribe.subs = subs
-            subscribe.author = author
-            subscribe.save()
-
-            return Response({}, status=status.HTTP_201_CREATED)
-
-    # User wants to Unsubscribe
-    elif request.method == "DELETE":
-        # handle the case if the client wants to 
-        # unfollow
-        try:
-            subs = Subscriptions.objects.get(pk=pk)
-        except Subscriptions.DoesNotExist:
-            return Response(
-                {'message': 'You do not follow this author'},
-                status=status.HTTP_400_BAD_REQUEST)
-
-        # Get the authenticated user
-        follower = RareUser.objects.get(user=request.auth.user)
-
-        try:
-            # try to unsubscribe
-            subs = Subscriptions.objects.get(
-                follower_id=user_id) # will need help with this
-            subs.delete()
-            return Response(None, status=status.HTTP_204_NO_CONTENT)
-
-        except Subscriptions.DoesNotExist:
-            return Response(
-                {'message': 'Not currently subscribed to author'},
-                status=status.HTTP_404_NOT_FOUND)
-
-    # If the client performs a request with a method that isnt
-    # POST or DELETE, tell the client the method is not supported
-    return Response({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
